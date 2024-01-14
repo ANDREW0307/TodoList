e4401fcf52a1128bc8a6e87623ad1711e08bec0c
devobjpro <devobj@gmail.com>
2024-01-14 09:54:36 -0500

    format

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
 
 interface Props {
   children: ReactNode;
   onClick: (event: React.MouseEvent) => void;
 }
 
-const Button = ({children, onClick}: Props) => {
-  return (
-    <button onClick={onClick}>{children}</button>  
-  )
-}
+const Button = ({ children, onClick }: Props) => {
+  return <button onClick={onClick}>{children}</button>;
+};
 
-export default Button
+export default Button;
